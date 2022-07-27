9cc5a92184f7c33ebe2b05b09986acf6a6157bd9
Ashim Shaw <shawashim72@gmail.com>
2022-07-27 22:20:58 +0530

    file 2

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -4,10 +4,11 @@ import React from 'react'
 export default function Welcome(props) {
   return (
     <div>
-        <details>
+        
             <h1>Hey ! {props.name}</h1>
             <h2>Welcome to Newton School.</h2>
-        </details>
+            
+        
     </div>
   )
 }
